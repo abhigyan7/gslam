8ba56d32becce607b64989742c4f82e0fffa7586
Abhigyan Bhusal <bhusalabhigyan@gmail.com>
2025-02-17 00:31:10 +0545

    Add code to remove keyframes but disable it now because it does not reduce redundancy as i had thought

--- gslam/pose_graph.py
+++ gslam/pose_graph.py
@@ -7,3 +7,10 @@ def add_constraint(pose_graph: defaultdict[int, set], kf1: int, kf2: int):
     pose_graph[kf1].add(kf2)
     pose_graph[kf2].add(kf1)
     return pose_graph
+
+
+def remove_keyframe(pose_graph: defaultdict[int, set], kf_id: int):
+    del pose_graph[kf_id]
+    for kf in pose_graph:
+        pose_graph[kf].discard(kf_id)
+    return pose_graph
